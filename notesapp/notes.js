6dddb718fef20f1b9c9cb05cfcc5e3964df552dd
MD SAMI <mdsami.diu@gmail.com>
2018-05-03 02:17:41 +0600

    working with json add note in json

--- notesapp/notes.js
+++ notesapp/notes.js
@@ -1,14 +1,28 @@
 console.log('starting note.js ');
 
-//console.log(module);
 
-// module.exports.age = 24;
-
-//module.exports.addNote = function ()
+const fs = require('fs');
 
 var addNote = (title, body) => {
 
-  console.log('Adding Note', title, body);
+  var notes = [];
+  var note = {
+
+    title,
+    body
+  };
+
+  try{
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  }catch (e) {
+
+  }
+
+
+
+  notes.push(note);
+  fs.writeFileSync('notes-data.json',JSON.stringify(notes));
 };
 
 var getAll = () => {
