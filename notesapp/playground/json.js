863a1ea33b50e130e405375a0e12a489da4d82f1
MD SAMI <mdsami.diu@gmail.com>
2018-05-03 01:49:26 +0600

    working with json add note

--- notesapp/playground/json.js
+++ notesapp/playground/json.js
@@ -8,8 +8,26 @@
 // console.log(typeof stringObj);
 // console.log(stringObj);
 
-var personString = '{"name": "mdsami", "age" :24}';
-var person = JSON.parse(personString);
+// var personString = '{"name": "mdsami", "age" :24}';
+// var person = JSON.parse(personString);
+//
+// console.log(typeof person);
+// console.log(person);
 
-console.log(typeof person);
-console.log(person);
+const fs = require('fs');
+
+var originalNote = {
+
+  title:'Some title',
+  body:'Some body'
+};
+
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json',originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString)
+
+console.log(typeof note);
+
+console.log(note.title);
